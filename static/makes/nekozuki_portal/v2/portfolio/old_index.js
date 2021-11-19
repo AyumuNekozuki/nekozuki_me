2833f54a7defef409fd8158c269485d427f0cb86
AyumuNekozuki <ayumu@nekozuki.me>
2021-11-19 12:48:21 +0900

    move devdependencies

---
+++ static/makes/nekozuki_portal/v2/portfolio/old_index.js
@@ -0,0 +1,14 @@
+//ボタンクリック時
+document.getElementById("2019").onclick = function(){
+  $('.workdiv').hide();
+  $('.2019').show();
+}
+
+document.getElementById("2020").onclick = function(){
+  $('.workdiv').hide();
+  $('.2020').show();
+}
+
+document.getElementById("all").onclick = function(){
+  $('.workdiv').show();
+}
---
+++ static/makes/nekozuki_portal/v4/README.md
@@ -0,0 +1,8 @@
+![](https://img.shields.io/github/license/AyumuNekozuki/nekozuki_me)
+
+![](https://nekozuki.me/img/favicon.png)
+
+# 猫月あゆむPORTAL(β)
+猫月あゆむのポータルサイトです。
+
+https://nekozuki.me/
