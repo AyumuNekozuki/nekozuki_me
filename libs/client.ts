17ff094eefd632a6c2b5ae942406e10524a7a9f8
AyumuNekozuki <ayumu@nekozuki.me>
2023-04-28 10:06:32 +0900

    reset

--- libs/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: 'nekozuki-me',
-  apiKey: process.env.API_KEY || '',
-});
--- libs/client_blog.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: 'nekolog',
-  apiKey: process.env.API_KEY_BLOG || '',
-});
